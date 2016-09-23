b13e01af2d114a156f3a489edc2a76f28d9bdbf7
bipeishan <bipeishan666@163.com>
2016-09-23 18:33:47 +0800

    调项目组下成员的接口

--- src/containers/project-mgr/actions/group_members_action.js
+++ src/containers/project-mgr/actions/group_members_action.js
@@ -2,13 +2,12 @@
  * Created by Administrator on 2016-09-23.
  */
 import api from '../../../api';
-import {GET_GROUPMEMBERS, } from '../constants/group-tree-types';
+import {GET_GROUPMEMBERS, } from '../constants/group_members_types';
 
 export function getGroupMembers(groupId) {
     var path = '/groups/members';
     //var path = 'http://10.10.156.110:11000/gitlab/groups/all';
     //var path = '/groups/all';
-    console.log("111");
     return {
         type: GET_GROUPMEMBERS,
         payload: {
--- src/containers/project-mgr/reducers/group_members_reducer.js
+++ src/containers/project-mgr/reducers/group_members_reducer.js
@@ -11,12 +11,10 @@ const initialState = {
 };
 
 export default function getGroupMembers(state = initialState, action = {}) {
-    console.log("4444");
     switch (action.type) {
         case GET_GROUPMEMBERS_PENDING:
             return Object.assign({}, initialState, {fetchStatus:false,} );
         case GET_GROUPMEMBERS_SUCCESS:
-            console.log("333");
             return Object.assign({}, initialState, {fetchStatus:true, groupMembers: action.payload});
         case GET_GROUPMEMBERS_ERROR:
             return {
