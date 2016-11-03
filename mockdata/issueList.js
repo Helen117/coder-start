af5f5d7283f3d957da6e2cb2c3555f962d8c1d5a
sunlei <sunlei@shpso.com>
2016-11-03 11:27:54 +0800

    issue table

--- mockdata/issueList.js
+++ mockdata/issueList.js
@@ -7,87 +7,101 @@ module.exports.issueList= {
     "errorMsg": null,
     "result": [
         {
-            key: 1,
-            "project_name": "devops_web",
-            "project_id": 17,
-            children: [
+            "key": "0_s",
+            "description": null,
+            "state": null,
+            "labels": null,
+            "sets_name": null,
+            "project_name": null,
+            "milestone_name": null,
+            "issue_name": null,
+            "assignee_name": null,
+            "author_name": null,
+            "assignee_id": null,
+            "author_id": null,
+            "user_notes_count": null,
+            "due_date": null,
+            "children": [
                 {
-                    key: 11,
-                    "milestone_id": 96,
-                    "milestoneTitle": "as",
-                    "milestone_due_date": 1477353600000,
-                    children: [
+                    "key": "186_p",
+                    "description": null,
+                    "state": null,
+                    "labels": null,
+                    "sets_name": null,
+                    "project_name": "devops_test",
+                    "milestone_name": null,
+                    "issue_name": null,
+                    "assignee_name": null,
+                    "author_name": null,
+                    "assignee_id": null,
+                    "author_id": null,
+                    "user_notes_count": null,
+                    "due_date": null,
+                    "children": [
                         {
-                            key: 111,
-                            "id": 46,
-                            "title": "test2",
-                            "description": "后台",
-                            "labels": [
-                                "测试label"
+                            "key": "0_m",
+                            "description": null,
+                            "state": null,
+                            "labels": null,
+                            "sets_name": null,
+                            "project_name": null,
+                            "milestone_name": null,
+                            "issue_name": null,
+                            "assignee_name": null,
+                            "author_name": null,
+                            "assignee_id": null,
+                            "author_id": null,
+                            "user_notes_count": null,
+                            "due_date": null,
+                            "children": [
+                                {
+                                    "key": "58_i",
+                                    "description": "test issue",
+                                    "state": "closed",
+                                    "labels":  [
+                                        "接口设计",
+                                        "测试label"
+                                    ],
+                                    "sets_name": null,
+                                    "project_name": null,
+                                    "milestone_name": null,
+                                    "issue_name": "新增问题",
+                                    "type":'demand',
+                                    "assignee_name": "孙磊",
+                                    "author_name": "孙磊",
+                                    "assignee_id": 9,
+                                    "author_id": 1,
+                                    "user_notes_count": null,
+                                    "due_date": 1477353600000,
+                                    "created_at":1476756934000,
+                                    "children": [
+                                        {
+                                            "key": "59_i",
+                                            "description": "需求下bug",
+                                            "state": "opened",
+                                            "type":'bug',
+                                            "labels":  [
+                                                "测试label"
+                                            ],
+                                            "sets_name": null,
+                                            "project_name": null,
+                                            "milestone_name": null,
+                                            "issue_name": "bug",
+                                            "assignee_name": "张亚军",
+                                            "author_name": "张亚军",
+                                            "assignee_id": 10,
+                                            "author_id": 1,
+                                            "user_notes_count": null,
+                                            "due_date": 1477353600000,
+                                            "created_at":1476756934000,
+                                        }
+                                    ],
+                                }
                             ],
-                            "assignee_name": "张亚军",
-                            "assignee_id": 8,
-                            "author_name": "孙磊",
-                            "author_id": 9,
-                            "state": "reopened",
-                            "updated_at": 1477298181000,
-                            "created_at": 1476090199000,
-                            "due_date": 1477353600000,
-                            "user_notes_count": 0
-                        }, {
-                            key: 112,
-                            "id": 45,
-                            "title": "test",
-                            "description": "devops",
-                            "labels": [],
-                            "assignee_name": "孙磊",
-                            "assignee_id": 9,
-                            "author_name": "赵静萍",
-                            "author_id": 9,
-                            "state": "opened",
-                            "updated_at": 1477015513000,
-                            "created_at": 1476756934000,
-                            "due_date": 1476835200000,
-                            "user_notes_count": 0,
                         }
-                    ]
+                    ],
                 }
-            ]
-        }, {
-            key: 2,
-            "project_id": 10,
-            "project_name": "devops_scm",
-            children: [
-                {
-                    key: 21,
-                    "milestone_id": 90,
-                    "milestoneTitle": "test",
-                    "milestone_due_date": 1477354600000,
-                    children: [
-                        {
-                            key: 211,
-                            "id": 48,
-                            "title": "test",
-                            "description": "devops_scm",
-                            "labels": [
-                                "接口设计",
-                                "测试label"
-                            ],
-                            "assignee_name": "孙磊",
-                            "assignee_id": 9,
-                            "author_name": "张亚军",
-                            "author_id": 9,
-                            "state": "reopened",
-                            "updated_at": 1477015513000,
-                            "created_at": 1476756934000,
-                            "due_date": 1476835200000,
-                            "user_notes_count": 0
-                        }
-                    ]
-
-                }
-            ]
-
+            ],
         }
     ]
 };
--- src/components/issues-list/index.js
+++ src/components/issues-list/index.js
@@ -6,10 +6,6 @@ import { Table ,Button,Input,notification } from 'antd';
 import './index.less';
 import Box from '../box';
 
-// const authName =[];
-// const assigneeName =[];
-// const label = [];
-// const milestone =[];
 export default class IssueList extends Component {
 
     constructor(props) {
@@ -60,9 +56,10 @@ export default class IssueList extends Component {
 
         let selectedRow = this.getListNode(selectedRows,this.props.dataSource);
         selectedRow.title = selectedRow.issue_name;
+        // console.log('row:',selectedRow);
         //console.log('window.location:',window.location);
         //查看我的问题不选择项目时不能新增问题
-        if(this.props.state=='myIssue' && !this.props.projectInfo){
+        if(this.props.state=='myIssue' && type=='add'&& !this.props.projectInfo){
             notification.error({
                 message: '未选择项目',
                 description:'请先在“代码管理“中选择一个项目！',
@@ -100,100 +97,28 @@ export default class IssueList extends Component {
     }
 
     //获取表格的数据源
-    getDataSource(issueList){
-        // console.log('issueList:',issueList);
-        const data = [];
-        if(issueList){
-            for (let i = 0; i < issueList.length; i++) {
-                var assign_name = issueList[i].assignee?issueList[i].assignee.name:null;
-                var assign_id = issueList[i].assignee?issueList[i].assignee.id:null;
-                var labels = issueList[i].labels&&issueList[i].labels.length>0?issueList[i].labels+'':null;
-                var milestoneTitle = issueList[i].milestone?issueList[i].milestone.title:null;
-                var milestone_id = issueList[i].milestone?issueList[i].milestone.id:null;
-                var milestoneDueDate = issueList[i].milestone?issueList[i].milestone.due_date:null;
-                data.push({
-                    //group_name:groupInfo.name,
-                    project_id:issueList[i].project_id,
-                    //project_name:projectInfo.name,
-                    id:issueList[i].id,
-                    title: issueList[i].title,
-                    description:issueList[i].description,
-                    author_name: issueList[i].author?issueList[i].author.name:null,
-                    author_username: issueList[i].author?issueList[i].author.username:null,
-                    assignee_name: assign_name,
-                    assign_id :assign_id,
-                    milestoneTitle:milestoneTitle,
-                    milestone_id:milestone_id,
-                    milestoneDueDate:milestoneDueDate,
-                    created_at:  this.getTime(issueList[i].created_at),
-                    due_date:this.getTime(issueList[i].due_date),
-                    labels: labels,
-                    state: issueList[i].state,
-                    login_username: this.props.loginInfo.username,
-                });
+    getDataSource(list){
+        // console.log('issueList:',list);
+        if(list) {
+            for (var i = 0; i < list.length; i++) {//项目集
+                for (var j = 0; j < list[i].children.length; j++) {//项目
+                    for (var k = 0; k < list[i].children[j].children.length; k++) {//里程碑
+                        for (var n = 0; n < list[i].children[j].children[k].children.length; n++) {//需求
+                                list[i].children[j].children[k].children[n].due_date = this.getTime(list[i].children[j].children[k].children[n].due_date);
+                                list[i].children[j].children[k].children[n].created_at = this.getTime(list[i].children[j].children[k].children[n].created_at);
+                                list[i].children[j].children[k].children[n].labels = list[i].children[j].children[k].children[n].labels && list[i].children[j].children[k].children[n].labels.length > 0 ? list[i].children[j].children[k].children[n].labels + '' : null;
 
-                //过滤创建人
-                var isRepeated = false;
-                for (var j = 0, len = authName.length; j < len; j++) {
-                    if (issueList[i].author&&issueList[i].author.name == authName[j].value) {
-                        isRepeated = true;
-                        break;
-                    }
-                }
-                if (!isRepeated&&issueList[i].author) {
-                    authName.push({text:issueList[i].author.name,
-                        value:issueList[i].author.name});
-                }
-
-                //过滤修复人
-                var flag = false;
-                for (var n = 0; n<assigneeName.length; n++) {
-                    if (assign_name && assign_name == assigneeName[n].value) {
-                        flag = true;
-                        break;
-                    }
-                }
-                if (!flag&&assign_name) {
-                    assigneeName.push({text:assign_name,
-                        value:assign_name});
-                }
-
-                //过滤问题标签
-                if(issueList[i].labels.length>0){
-                    var isLabel = false;
-                    for(var l=0;l<issueList[i].labels.length;l++){
-                        for (var m = 0; m<label.length; m++) {
-                            if ( issueList[i].labels[l] == label[m].value) {
-                                isLabel = true;
-                                break;
-                            }
-                        }
-                        if (!isLabel) {
-                            label.push({text:issueList[i].labels[l],
-                                value:issueList[i].labels[l]});
+                                for (var m = 0; m < list[i].children[j].children[k].children[n].children.length; m++) {//bug
+                                    list[i].children[j].children[k].children[n].children[m].due_date = this.getTime(list[i].children[j].children[k].children[n].children[m].due_date);
+                                    list[i].children[j].children[k].children[n].children[m].created_at = this.getTime(list[i].children[j].children[k].children[n].children[m].created_at);
+                                    list[i].children[j].children[k].children[n].children[m].labels = list[i].children[j].children[k].children[n].children[m].labels && list[i].children[j].children[k].children[n].children[m].labels.length > 0 ? list[i].children[j].children[k].children[n].children[m].labels + '' : null;
+                                }
                         }
                     }
-
                 }
-
-                //过滤里程碑
-                var isflag = false;
-                for (var s = 0; s<milestone.length; s++) {
-                    if (milestoneTitle && milestoneTitle == milestone[s].value) {
-                        isflag = true;
-                        break;
-                    }
-                }
-                if (!isflag&&milestoneTitle) {
-                    milestone.push({text:milestoneTitle,
-                        value:milestoneTitle});
-                }
-
-
             }
-
         }
-        return data;
+        return list;
     }
 
     rowClassName(record, index) {
@@ -217,7 +142,7 @@ export default class IssueList extends Component {
         return (
             <Box title="问题列表信息" >
                 <Button type="primary" onClick={this.editIssue.bind(this,'add',null)}>新增问题</Button>
-                <Table columns={this.issueListColumns(this)} dataSource={this.props.dataSource}
+                <Table columns={this.issueListColumns(this)} dataSource={this.getDataSource(this.props.dataSource)}
                        bordered
                        size="middle"
                        loading={this.props.loading}
@@ -271,16 +196,10 @@ IssueList.prototype.issueListColumns = (self)=>[
     title: '里程碑',
     dataIndex: 'milestone_name',
     width: '8%',
-    // filters:milestone,
-    // onFilter: (value, record) => record.milestoneTitle && record.milestoneTitle.indexOf(value) === 0,
-    // sorter: (a, b) => a.milestone_due_date > b.milestone_due_date
 },{
         title: '问题类型',
         dataIndex: 'type',
         width: '8%',
-        // filters:label,
-        // onFilter: (value, record) => record.labels && record.labels.indexOf(value) != -1,
-        // sorter: (a, b) => a.labels&&b.labels&&(a.labels.length - b.labels.length)
 },{
     title: '问题名称',
     dataIndex: 'issue_name',
@@ -293,21 +212,14 @@ IssueList.prototype.issueListColumns = (self)=>[
         title: '问题标签',
         dataIndex: 'labels',
         width: '8%',
-        // filters:label,
-        // onFilter: (value, record) => record.labels && record.labels.indexOf(value) != -1,
-        // sorter: (a, b) => a.labels&&b.labels&&(a.labels.length - b.labels.length)
 }, {
     title: '创建人',
     dataIndex: 'author_name',
     width: '7%',
-    // filters:authName,
-    // onFilter: (value, record) => record.author_name.indexOf(value) === 0
 },{
     title: '修复人',
     dataIndex: 'assignee_name',
     width: '7%',
-    // filters:assigneeName,
-    // onFilter: (value, record) => record.assignee_name && record.assignee_name.indexOf(value) === 0
 }, {
     title: '问题创建时间',
     dataIndex: 'created_at',
@@ -320,17 +232,6 @@ IssueList.prototype.issueListColumns = (self)=>[
     title: '状态',
     dataIndex: 'state',
     width: '8%',
-    // filters: [{
-    //     text: 'closed',
-    //     value: 'closed',
-    // }, {
-    //     text: 'opened',
-    //     value: 'opened',
-    // },{
-    //     text: 'reopened',
-    //     value: 'reopened',
-    // }],
-    // onFilter: (value, record) => record.state.indexOf(value) === 0,
 },{
     title: '操作',
     dataIndex: 'key',
