970152cc2eec4a3085eef1c7507690bfde5e1db2
bipeishan <bipeishan666@163.com>
2016-11-18 14:25:37 +0800

    ddd

---
+++ src/containers/user-relation/update-basic-info.js
@@ -0,0 +1,164 @@
+/**
+ * Created by Administrator on 2016-11-18.
+ */
+import React, { PropTypes } from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {Form, Input, Button, notification,Modal,Select} from 'antd';
+import 'pubsub-js';
+import {UpdateUser} from './actions/user-detail-action';
+
+const FormItem = Form.Item;
+const confirm = Modal.confirm;
+const Option = Select.Option;
+
+class UpdateBasicInfo extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const { form,loginInfo,UpdateUser } = this.props;
+        form.validateFields((errors, values) => {
+            if (!!errors) {
+                return;
+            } else {
+                const formData = form.getFieldsValue();
+                let data = {};
+                data.user_id = loginInfo.userId;
+                data.password = formData.old_password;
+                data.new_password = formData.new_password;
+                //调修改成员信息接口
+                UpdateUser(data);
+            }
+        })
+    }
+
+    handleCancel() {
+        const {form} = this.props;
+        const {router} = this.context;
+
+        confirm({
+            title: '您是否确定要取消表单的编辑',
+            content: '取消之后表单内未提交的修改将会被丢弃',
+            onOk() {
+                router.goBack();
+                form.resetFields();
+            },
+            onCancel() {
+            }
+        })
+    }
+
+    insertCallback(message){
+        notification.success({
+            message: message,
+            description: '',
+            duration: 2
+        });
+        this.context.router.goBack();
+    }
+
+    errCallback(message,errmessage){
+        notification.error({
+            message: message,
+            description:errmessage,
+            duration: 4
+        });
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const {updateResult, updateErrors} = nextProps;
+        //修改返回信息
+        if (this.props.updateResult != updateResult && updateResult) {
+            this.insertCallback("修改成功");
+        } else if (this.props.updateErrors != updateErrors && updateErrors) {
+            this.errCallback("修改失败",updateErrors);
+        }
+    }
+
+    checkEmail(rule, value, callback){
+        var reg = /^[a-z0-9_\.\-]*$/;
+        if (!value) {
+            callback();
+        } else {
+            if (!reg.test(value)) {
+                callback('请输入正确的邮箱！');
+            } else {
+                callback();
+            }
+        }
+    }
+
+    render() {
+        const {visible} = this.props;
+        const {getFieldDecorator} = this.props.form;
+        const formItemLayout = {
+            labelCol: {span: 5},
+            wrapperCol: {span: 8},
+        };
+        const nameProps = getFieldDecorator('name',
+            {rules:[
+                {required:true, message:'请输入中文名！'},
+            ]})(<Input type="text" placeholder="请输入中文名"/>);
+        const selectAfter = getFieldDecorator('option',{initialValue:'@shpso.com'})(
+            <Select style={{ width: 100 }}>
+                <Option value="@shpso.com">@shpso.com</Option>
+                <Option value="@boss.com">@boss.com</Option>
+            </Select>
+        );
+
+        if(visible == true){
+            return(
+                <Form horizontal onSubmit={this.handleSubmit.bind(this)}>
+                    <FormItem {...formItemLayout} label="中文名">
+                        {nameProps}
+                    </FormItem>
+
+                    <FormItem {...formItemLayout}  label="邮箱" >
+                        {getFieldDecorator('email',{rules:[{
+                            required:true,message:'不能为空'},{validator:this.checkEmail}]})(
+                            <Input placeholder="邮箱" addonAfter={selectAfter}/>
+                        )}
+                    </FormItem>
+
+                    <FormItem wrapperCol={{span: 10, offset: 7}} style={{marginTop: 24}}>
+                        <Button type="primary" htmlType="submit"
+                                loading={this.props.updateLoading}
+                                disabled={this.props.updateDisabled}>
+                            确定</Button>
+                        <Button type="ghost" onClick={this.handleCancel.bind(this)}>重置</Button>
+                    </FormItem>
+                </Form>
+            )
+        }else {return <div></div>}
+    }
+}
+
+UpdateBasicInfo.contextTypes = {
+    history: PropTypes.object.isRequired,
+    router: PropTypes.object.isRequired,
+    store: PropTypes.object.isRequired
+};
+
+
+UpdateBasicInfo = Form.create()(UpdateBasicInfo);
+
+function mapStateToProps(state) {
+    return {
+        loginInfo:state.login.profile,
+        updateResult:state.createUser.updateResult,
+        updateErrors:state.createUser.updateErrors,
+        updateLoading:state.createUser.updateLoading,
+        updateDisabled:state.createUser.updateDisabled,
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        UpdateUser:bindActionCreators(UpdateUser, dispatch),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UpdateBasicInfo);
---
+++ src/containers/user-relation/update-sshkey.js
@@ -0,0 +1,166 @@
+/**
+ * Created by Administrator on 2016-11-18.
+ */
+import React, { PropTypes } from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {Form, Input, Button, notification,Modal} from 'antd';
+import 'pubsub-js';
+import {UpdateUser} from './actions/user-detail-action';
+
+const FormItem = Form.Item;
+const confirm = Modal.confirm;
+
+class UpdateSshKey extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const { form,loginInfo,UpdateUser } = this.props;
+        form.validateFields((errors, values) => {
+            if (!!errors) {
+                return;
+            } else {
+                const formData = form.getFieldsValue();
+                let data = {};
+                data.user_id = loginInfo.userId;
+                data.password = formData.old_password;
+                data.new_password = formData.new_password;
+                //调修改成员信息接口
+                UpdateUser(data);
+            }
+        })
+    }
+
+    handleCancel() {
+        const {form} = this.props;
+        const {router} = this.context;
+
+        confirm({
+            title: '您是否确定要取消表单的编辑',
+            content: '取消之后表单内未提交的修改将会被丢弃',
+            onOk() {
+                router.goBack();
+                form.resetFields();
+            },
+            onCancel() {
+            }
+        })
+    }
+
+    insertCallback(message){
+        notification.success({
+            message: message,
+            description: '',
+            duration: 2
+        });
+        this.context.router.goBack();
+    }
+
+    errCallback(message,errmessage){
+        notification.error({
+            message: message,
+            description:errmessage,
+            duration: 4
+        });
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const {updateResult, updateErrors} = nextProps;
+        //修改返回信息
+        if (this.props.updateResult != updateResult && updateResult) {
+            this.insertCallback("修改成功");
+        } else if (this.props.updateErrors != updateErrors && updateErrors) {
+            this.errCallback("修改失败",updateErrors);
+        }
+    }
+
+    comfirmNewPass(rule, value, callback){//确认新密码
+        const {getFieldValue} = this.props.form;
+        if (value && value !== getFieldValue('new_password')) {
+            callback('两次密码输入不一致!');
+        } else {
+            callback();
+        }
+    }
+
+    render() {
+        const {visible} = this.props;
+        const {getFieldDecorator} = this.props.form;
+        const formItemLayout = {
+            labelCol: {span: 5},
+            wrapperCol: {span: 8},
+        };
+        const oldPasswordProps = getFieldDecorator('old_password',
+            {rules:[
+                {required:true, message:'请输入原始密码！'},
+            ]})(<Input type="password" placeholder="请输入原密码"/>);
+        const newPasswordProps = getFieldDecorator('new_password',
+            {rules:[
+                {required:true, message:'请输入新密码！'},
+            ]})(<Input type="password" placeholder="请输入新密码"/>);
+        const comfirmNewPassword = getFieldDecorator('comfirm_password',
+            {rules:[
+                {required:true, message:'请输入新密码！'},
+                {validator: this.comfirmNewPass.bind(this)}
+            ]})(<Input type="password" placeholder="请再次输入新密码"/>);
+
+        if(visible == true){
+            return(
+                <div>
+                    <span>帮助信息：</span>
+                    <Form horizontal onSubmit={this.handleSubmit.bind(this)}>
+                        <FormItem {...formItemLayout} label="原密码">
+                            {oldPasswordProps}
+                        </FormItem>
+
+                        <FormItem {...formItemLayout} label="新密码">
+                            {newPasswordProps}
+                        </FormItem>
+
+                        <FormItem {...formItemLayout} label="确认新密码">
+                            {comfirmNewPassword}
+                        </FormItem>
+
+                        <FormItem wrapperCol={{span: 10, offset: 7}} style={{marginTop: 24}}>
+                            <Button type="primary" htmlType="submit"
+                                    loading={this.props.updateLoading}
+                                    disabled={this.props.updateDisabled}>
+                                确定</Button>
+                            <Button type="ghost" onClick={this.handleCancel.bind(this)}>取消</Button>
+                        </FormItem>
+                    </Form>
+                </div>
+            )
+        }else {return <div></div>}
+    }
+}
+
+UpdateSshKey.contextTypes = {
+    history: PropTypes.object.isRequired,
+    router: PropTypes.object.isRequired,
+    store: PropTypes.object.isRequired
+};
+
+
+UpdateSshKey = Form.create()(UpdateSshKey);
+
+function mapStateToProps(state) {
+    return {
+        loginInfo:state.login.profile,
+        updateResult:state.createUser.updateResult,
+        updateErrors:state.createUser.updateErrors,
+        updateLoading:state.createUser.updateLoading,
+        updateDisabled:state.createUser.updateDisabled,
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        UpdateUser:bindActionCreators(UpdateUser, dispatch),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UpdateSshKey);
