e51a5969d5ea16686ff614285b2cb24c782fa128
bipeishan <bipeishan666@163.com>
2016-11-23 16:23:23 +0800

    index

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,11 @@
 import Api from './api';
 
 const api = new Api({
-    //baseURI: 'http://10.10.152.144:11000/gitlab',
+    baseURI: 'http://10.10.152.144:11000/gitlab',
     ciBaseURI: 'http://10.10.152.144:13000/jenkins',
 
     //baseURI: 'http://10.10.156.46:11000/gitlab',
-    baseURI: 'http://10.10.156.137:11000/gitlab',
+    //baseURI: 'http://10.10.156.137:11000/gitlab',
     //baseURI: '/gitlab',
 
     headers: {
