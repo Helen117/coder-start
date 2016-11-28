a0ef659fb5fb00e14b6ba47968082b247222e6a8
bipeishan <bipeishan666@163.com>
2016-11-28 17:01:08 +0800

    调度组件

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,13 @@
 import Api from './api';
 
 const api = new Api({
-    baseURI: 'http://10.10.152.144:11000/gitlab',
+    //baseURI: 'http://10.10.152.144:11000/gitlab',
     //baseURI: 'http://10.10.156.181:11000/gitlab',
     ciBaseURI: 'http://10.10.152.144:13000/jenkins',
 
     //baseURI: 'http://10.10.156.46:11000/gitlab',
     //baseURI: 'http://10.10.156.181:11000/gitlab',
-    //baseURI: '/gitlab',
+    baseURI: '/gitlab',
 
     headers: {
         //'Accept': 'application/json',
