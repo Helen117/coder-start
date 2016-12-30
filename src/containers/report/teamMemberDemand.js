957b8081acc12e6f47b5fa87ef0d5a7633839dc2
sunlei <sunlei@shpso.com>
2016-12-30 16:55:29 +0800

    报表优化

--- src/containers/report/teamMemberDemand.js
+++ src/containers/report/teamMemberDemand.js
@@ -5,6 +5,7 @@ import React, {PropTypes,Component} from 'react';
 import ReactEcharts from 'echarts-for-react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
+import { Row,Col} from 'antd';
 import * as reportActions from './report-action';
 import Box from '../../components/box';
 
@@ -16,19 +17,19 @@ class TeamMemberDemandProportion extends Component {
     }
 
     componentWillMount(){
-        const { actions } = this.props;
-        actions.fetchreportData();
+        // const { actions } = this.props;
+        // actions.fetchreportData();
     }
 
     componentWillReceiveProps(nextProps) {
 
     }
 
-    getOption() {
+    getDemandOption() {
         const option = {
-            // title: {
-            //     text: '待办事项'
-            // },
+            title: {
+                text: '需求数量'
+            },
             tooltip : {
                 trigger: 'item',
                 formatter: "{a} <br/>{b} : {c} ({d}%)"
@@ -39,20 +40,90 @@ class TeamMemberDemandProportion extends Component {
                 right:'right',
                 data:['成员1','成员2','成员3','成员4','成员5'],
             },
-            // toolbox: {
-            //     orient:"vertical",
-            //     y:'center',
-            //     itemGap:15,
-            //     x:'right',
-            //     show : true,
-            //     feature : {
-            //         saveAsImage: {},
-            //         dataView : {show: true, readOnly: false}
-            //     }
-            // },
             series : [
                 {
                     name: '需求数量',
+                    type: 'pie',
+                    radius : '55%',
+                    center: ['50%', '60%'],
+                    data:[
+                        {value:3, name:'成员1'},
+                        {value:5, name:'成员2'},
+                        {value:6, name:'成员3'},
+                        {value:2, name:'成员4'},
+                        {value:1, name:'成员5'}
+                    ],
+                    itemStyle: {
+                        emphasis: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }],
+        };
+        return option;
+    }
+
+    getDefectOption() {
+        const option = {
+            title: {
+                text: '缺陷数量'
+            },
+            tooltip : {
+                trigger: 'item',
+                formatter: "{a} <br/>{b} : {c} ({d}%)"
+            },
+            legend:{
+                orient:"vertical",
+                y:"center",
+                right:'right',
+                data:['成员1','成员2','成员3','成员4','成员5'],
+            },
+            series : [
+                {
+                    name: '缺陷数量',
+                    type: 'pie',
+                    radius : '55%',
+                    center: ['50%', '60%'],
+                    data:[
+                        {value:3, name:'成员1'},
+                        {value:5, name:'成员2'},
+                        {value:6, name:'成员3'},
+                        {value:2, name:'成员4'},
+                        {value:1, name:'成员5'}
+                    ],
+                    itemStyle: {
+                        emphasis: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]
+        };
+        return option;
+    }
+
+    getOvertimeOption() {
+        const option = {
+            title: {
+                text: '超时数量'
+            },
+            tooltip : {
+                trigger: 'item',
+                formatter: "{a} <br/>{b} : {c} ({d}%)"
+            },
+            legend:{
+                orient:"vertical",
+                y:"center",
+                right:'right',
+                data:['成员1','成员2','成员3','成员4','成员5'],
+            },
+            series : [
+                {
+                    name: '超时数量',
                     type: 'pie',
                     radius : '55%',
                     center: ['50%', '60%'],
@@ -79,11 +150,21 @@ class TeamMemberDemandProportion extends Component {
     render() {
         return(
             <Box title="从团队leader视角展示当前团队成员的需求、超时工单和缺陷的占比情况分析">
-                <ReactEcharts
-                    option={this.getOption()}
-                    style={{height: '350px', width: '80%'}}
-                    theme="my_theme"
-                />
+
+                <Row>
+                    <Col span={12}>
+                        <ReactEcharts option={this.getDemandOption()}/>
+                    </Col>
+                    <Col span={12} style={{textAlign:'right'}}>
+                        <ReactEcharts option={this.getDefectOption()} />
+                    </Col>
+                </Row>
+                <Row>
+                    <ReactEcharts
+                        option={this.getOvertimeOption()}
+                        style={{height: '350px', width: '80%'}}
+                    />
+                </Row>
             </Box>
         );
 
