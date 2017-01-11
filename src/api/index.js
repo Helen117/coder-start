73753de563a22de0d23a8c6f8abdc261438e480d
sunlei <sunlei@shpso.com>
2017-01-11 19:00:54 +0800

    mergeRequest验证

--- src/api/index.js
+++ src/api/index.js
@@ -4,9 +4,7 @@ const api = new Api({
     ciBaseURI: 'http://10.10.147.190:13000/jenkins',
     // baseURI: '/gitlab',
     baseURI: 'http://10.10.147.190:11000/gitlab',
-    //baseURI: 'http://10.10.156.108:11000/gitlab';
-    //add new code
-
+    // baseURI: 'http://10.10.156.23:11000/gitlab',
 
     headers: {
         //'Accept': 'application/json',
