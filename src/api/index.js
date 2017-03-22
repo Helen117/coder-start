31b7ca3e1ffdfdf2f65b41f63ea0efd0787ffd98
bips <bips@shpso.com>
2017-03-22 19:47:44 +0800

    1

--- src/api/index.js
+++ src/api/index.js
@@ -2,9 +2,9 @@ import Api from './api';
 
 const api = new Api({
     ciBaseURI: 'http://10.10.147.190:13000/jenkins',
-    //baseURI: 'http://10.10.147.190:11000/gitlab',
-    //baseURI: 'http://10.10.156.59:11000/gitlab',
-    baseURI: '/gitlab',
+    baseURI: 'http://10.10.147.190:11000/gitlab',
+    //baseURI: 'http://10.10.167.69:11000/gitlab',
+    //baseURI: '/gitlab',
 
 
     headers: {
