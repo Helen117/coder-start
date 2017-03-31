f283b03d41bead2a7889ed15e6a540febbcd3136
bips <bips@shpso.com>
2017-03-31 15:43:38 +0800

    11

--- src/containers/task-board/index.js
+++ src/containers/task-board/index.js
@@ -7,19 +7,20 @@ import {connect} from 'react-redux';
 import { Button, Row, Col } from 'antd';
 import 'pubsub-js';
 import AsyncTree from './async-tree';
+import EditStory from '../story/editStory';
 
 class TaskBoard extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-
+            editStoryVisible:false
         };
     }
 
     componentDidMount() {
 
     }
-//state:{"storyId": storyId}
+
     selectedNode(node){
         console.log('node:',node)
         this.context.router.push({
@@ -28,16 +29,33 @@ class TaskBoard extends React.Component{
         });
     }
 
+    addStory(visible){
+        this.setState({
+            editStoryVisible:visible
+        })
+    }
+
+    setVisible(visible){
+        this.setState({
+            editStoryVisible:visible
+        })
+    }
+
 
     render(){
+
+        console.log('this.state.editStoryVisible:',this.state.editStoryVisible)
 
         return (
             <Row className="ant-layout-content" style={{minHeight:300}}>
                 <Col span={6}>
-                    <AsyncTree onSelect={this.selectedNode.bind(this)}/>
+                    <AsyncTree onSelect={this.selectedNode.bind(this)}
+                        clickAdd={this.addStory.bind(this)}/>
                 </Col>
                 <Col span={18}>
                     {this.props.children}
+                    <EditStory visible={this.state.editStoryVisible} editType="add"
+                               setVisible={this.setVisible.bind(this)}/>
                 </Col>
             </Row>
         );
